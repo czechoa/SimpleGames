7d75565f6075abb603b51e936bb5fdb01ff775ac
czechoa <andrzej.czechu.ja@gmail.com>
2020-07-09 17:17:29 +0200

    dodanie Initialize, które wykonuje sie od razu

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class Controller {
+public class Controller implements Initializable {
     public Button button;
-
+    public Label firstLabel;
+    String message = "";
     public void handleButtonClick(ActionEvent actionEvent) {
         System.out.println("somthink happen ");
         button.setText("don't touch me");
+        message += "trlala";
+        firstLabel.setText(message);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("loading user data ..  "); // wykonuje sie od razu
     }
 }
