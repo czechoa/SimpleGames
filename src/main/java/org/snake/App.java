c2b6dc440eedb455561bab3d63352c3157a97736
czechoa <andrzej.czechu.ja@gmail.com>
2020-07-22 22:28:36 +0200

    przeniesienie App do AppSnake oraz dodanie wczytuwannie sample( do poprawy)

--- src/main/java/org/snake/App.java
+++ src/main/java/org/snake/App.java
@@ -1,9 +1,12 @@
 package org.snake;
 
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.input.KeyCode;
@@ -14,53 +17,28 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
+    private static Stage stage;
     private int boardXSize;
     private int boardYSize;
     private int snakePartSize;
     private int snakeAmountPart;
     private int fruitSize;
+
     @Override
-    public void start(Stage stage) {
-
-        List<String> list = Reader.readerFile(Objects.requireNonNull(getClass().getClassLoader().getResource("dataToSnake")).getFile());
-
-        boardXSize = getValueFromList(list,0);
-        boardYSize = getValueFromList(list,1);
-        snakePartSize = getValueFromList(list,2);
-        snakeAmountPart = getValueFromList(list,3);
-        fruitSize = getValueFromList(list,4);
-
-        HBox hBox = new HBox();
-        Canvas canvas = new Canvas(boardXSize, boardYSize);
-
-        hBox.getChildren().add(canvas);
-        Snake snake = new Snake(boardXSize, boardYSize,snakePartSize,snakeAmountPart,fruitSize);
-        RunLoop runLoop = new RunLoop(canvas, snake);
-        runLoop.start();
-
-        Scene scene = new Scene(hBox, boardXSize, boardYSize);
-        scene.addEventFilter(KeyEvent.KEY_PRESSED, key -> {
-            if (key.getCode() == KeyCode.W) {
-                snake.setDirection(Direction.UP);
-            }
-            if (key.getCode() == KeyCode.A) {
-                snake.setDirection(Direction.LEFT);
-            }
-            if (key.getCode() == KeyCode.S) {
-                snake.setDirection(Direction.DOWN);
-            }
-            if (key.getCode() == KeyCode.D) {
-                snake.setDirection(Direction.RIGHT);
-            }
-
-        });
+    public void start(Stage stage) throws IOException {
+        this.stage = stage;
+        Scene scene = new Scene(loadFXML("sample"));
         stage.setScene(scene);
-        stage.show();
-
     }
-    private int getValueFromList(List<String> list,int numberIndex){
-        String[] oneLine = list.get(numberIndex).split("\\s+");
-        return Integer.parseInt(oneLine[1]);
+
+    static void setRoot(String fxml) throws IOException {
+//        scene.setRoot(loadFXML(fxml));
+        new AppSnake(stage);
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
 
