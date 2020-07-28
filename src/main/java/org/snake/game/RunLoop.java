cfe5216cd4f3812533e0b085abc49e380290e691
czechoa <andrzej.czechu.ja@gmail.com>
2020-07-28 18:19:17 +0200

    dodanie czasu do score

--- src/main/java/org/snake/game/RunLoop.java
+++ src/main/java/org/snake/game/RunLoop.java
@@ -1,5 +1,7 @@
 package org.snake.game;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -9,20 +11,23 @@ import org.snake.WriterReaderFileScore;
 
 public class RunLoop extends AnimationTimer {
 
-    private Canvas canvas;
-    private int speed = 3;
+    private final double scoreStringSize;
+    private final Canvas canvas;
+    private int speed;
     private final GraphicsContext graphics;
-    long  lastTick = 0;
-    Snake snake;
-    private double stringSize;
-    private String namePlayer = "Player 1";
+    private long lastTick = 0;
+    private final Snake snake;
+    private final double stringSize;
+    private final String namePlayer = "Player 1";
 
 
-    RunLoop(Canvas canvas, Snake snake){
+    RunLoop(Canvas canvas, Snake snake) {
         this.canvas = canvas;
         graphics = canvas.getGraphicsContext2D();
         this.snake = snake;
-        stringSize = Math.sqrt(canvas.getWidth()*canvas.getHeight())/8;
+        stringSize = Math.sqrt(canvas.getWidth() * canvas.getHeight()) / 8;
+        scoreStringSize = stringSize / 2;
+
     }
 
     @Override
@@ -33,35 +38,44 @@ public class RunLoop extends AnimationTimer {
             return;
         }
 
-        if (now - lastTick > 1000000000 / (speed + 4 )) {
+        if (now - lastTick > 1000000000 / (speed + 4)) {
             lastTick = now;
             tick();
             speed = snake.getLevel();
         }
     }
-    private void tick(){
+
+    private void tick() {
         graphics.setFill(Color.BLACK);
-        graphics.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
-        writeStringToScreen("score :" +snake.getLevel(),Color.YELLOW);
-        snake.tick(graphics);
-        if(!snake.isAlive()){
-            graphics.setFill(Color.BLACK);
-            graphics.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
-            String gameOver = "GAME OVER \n Score  "  + snake.getLevel();
-            writeStringToScreen(gameOver,Color.RED);
+        graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        if (snake.isAlive()) {
+            snake.tick(graphics);
+            graphics.setFill(Color.WHITE);
+            graphics.setFont(new Font("", scoreStringSize));
+            graphics.fillText("score: " + speed, 0, scoreStringSize);
+        } else {
+            String gameOver = "GAME OVER \n Score  " + snake.getLevel();
+            writeStringToScreen(gameOver, Color.RED);
             stop();
-            WriterReaderFileScore.write(" " +snake.getLevel());
+            WriterReaderFileScore.write(getCurrentTimeStamp() + " " + snake.getLevel());
         }
     }
 
-    public void pressSpaceToStartGame(){
+    public void pressSpaceToStartGame() {
         tick();
-        writeStringToScreen("Press Space \n to Start ",Color.GREEN);
+        writeStringToScreen("Press Space \n to Start ", Color.GREEN);
     }
-    private void writeStringToScreen(String string, Color color){
+
+    private void writeStringToScreen(String string, Color color) {
         graphics.setFill(color);
         graphics.setFont(new Font("", stringSize));
-        graphics.fillText(string, canvas.getWidth()/8, canvas.getHeight()/2);
+        graphics.fillText(string, canvas.getWidth() / 8, canvas.getHeight() / 2);
+    }
+    public String getCurrentTimeStamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        return strDate;
     }
 
 
