c60066e14d42de5fe9b9275e11352710caa65c42
czechoa <andrzej.czechu.ja@gmail.com>
2020-07-30 20:49:49 +0200

    aby dział wyswietlenie scoru

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens org.snake to javafx.base;
     opens org.snake.controller to javafx.fxml;
     exports org.snake.controller;
 }
